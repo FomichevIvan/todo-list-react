a22f80906f2a24ee4cc36363e8be2c2b25337346
Ivan Fomichev <yonderboy@yandex.ru>
2022-05-18 07:17:03 -0700

    refactor

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,19 +2,20 @@ import { memo } from 'react';
 import { MdDone, MdOutlineDoneAll, MdOutlineClose } from 'react-icons/md';
 
 function Todo({ todo }) {
+  const { date, text, id, status, handleDone, handleDelete } = todo;
   return (
     <div className={'block-container'}>
-      <div>{todo.date}</div>
-      <div>{todo.text}</div>
-      <div onClick={() => todo.handleDone(todo.id)}>
-        {todo.status ? (
+      <div>{date}</div>
+      <div>{text}</div>
+      <div onClick={() => handleDone(id)}>
+        {status ? (
           <MdOutlineDoneAll className={'icon-done'} />
         ) : (
           <MdDone className={'icon-undone'} />
         )}{' '}
       </div>
       <div>
-        <MdOutlineClose className={'icon-delete'} onClick={() => todo.handleDelete(todo.id)} />
+        <MdOutlineClose className={'icon-delete'} onClick={() => handleDelete(id)} />
       </div>
     </div>
   );
